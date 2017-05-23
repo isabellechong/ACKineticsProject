12c8d2b7c6d0fb86714f3ef3f3e51e4eb177b525
Engt <engt17byramhills@gmail.com>
2017-05-23 13:40:34 -0400

    Add files via upload

---
+++ Arduino/trapdoor_shifter.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo trapdoor;
+int trapPin = 10;
+
+void setup() {
+  // put your setup code here, to run once:
+  trapdoor.attach(10);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  trapdoor.write(10);
+  delay(50);
+  trapdoor.write(15);
+  delay(50);
+  trapdoor.write(3);
+  delay(5000);
+}
